3151b759a3190d6458823632d46712ed6f0d9bfc
Dmytro Petyshyn <dev6012@gmail.com>
2023-12-06 17:20:34 +0200

    add tg field for Contact model

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -736,6 +736,12 @@ export interface ApiContactContact extends Schema.CollectionType {
           localized: false;
         };
       }>;
+    tg: Attribute.String &
+      Attribute.SetPluginOptions<{
+        i18n: {
+          localized: false;
+        };
+      }>;
     createdAt: Attribute.DateTime;
     updatedAt: Attribute.DateTime;
     publishedAt: Attribute.DateTime;
